bb9a6816a777b0ca5ebf0020112d398285e71393
Alexander Er <aceszhenwei@yahoo.com>
2021-06-25 21:55:13 +0800

    Options

--- Overhaul/Scripts/LevelsSel.gd
+++ Overhaul/Scripts/LevelsSel.gd
@@ -7,29 +7,24 @@ onready var selectorOne = $"CenterContainer/VBoxContainer/CenterContainer2/HBoxC
 onready var selectorTwo = $"CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer/Level 2"
 onready var selectorThree = $"CenterContainer/VBoxContainer/CenterContainer2/HBoxContainer/Level 3"
 
-
-
-
-# Code to add in!
-func _unlock_Level(level):
-	pass
+func _unlock_Level():
+	if GlobalSettings.unlockedlevels.has(2):
+		selectorTwo.disabled = false
+	if GlobalSettings.unlockedlevels.has(3):
+		selectorThree.disabled = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	_unlock_Level()
 
 func _on_Level_1_pressed():
 	get_parent().add_child(lvlOne.instance())
 	queue_free()
 
-
 func _on_Level_2_pressed():
 	get_parent().add_child(lvlTwo.instance())
 	queue_free()
+
+func _on_Level_3_pressed():
+	pass
