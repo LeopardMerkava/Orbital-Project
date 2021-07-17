2ac6a948c4be601f3be044a63981dfbe78077c59
LeopardMerkava <andrewbaruch4@gmail.com>
2021-07-17 20:49:16 +0800

    Added AA tower
    Floaters now can only be shot by aa towers :)

---
+++ Projectiles/AAProjectile.gd
@@ -0,0 +1,9 @@
+extends Projectile
+
+export var damage_dealt : int = 2
+export var travel_speed : int = 800
+
+func _ready() -> void:
+	.set_damage(damage_dealt)
+	.set_speed(travel_speed)
+	
---
+++ Towers/AATower.gd
@@ -0,0 +1,9 @@
+extends Towers
+
+var cost = 5
+
+func _ready() -> void:
+	.set_projectile(load("res://Projectiles/AAProjectile.tscn"))
+	$TowerRange.connect("area_entered", self, "_on_Tower_Range_area_entered")
+	$TowerRange.connect("area_exited", self, "_on_Tower_Range_area_exited")
+
